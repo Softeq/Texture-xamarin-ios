ff139f7d870a75f18f35a6516795e4177ecbb455
Yauheni Pakala <evgeniy.pakalo@gmail.com>
2022-11-21 15:56:17 +0200

    Add sample

---
+++ samples/DataModel.cs
@@ -0,0 +1,13 @@
+﻿// Developed by Softeq Development Corporation
+// http://www.softeq.com
+
+namespace AsyncDisplayKitSamples;
+
+public partial class ViewController
+{
+    public class DataModel
+    {
+        public string Title { get; set; }
+        public string Image { get; set; }
+    }
+}
--- samples/ViewController.cs
+++ samples/ViewController.cs
@@ -16,7 +16,47 @@ public partial class ViewController : UIViewController
     public override void ViewDidLoad()
     {
         base.ViewDidLoad();
-        // Perform any additional setup after loading the view, typically from a nib.
+
+        // add AsyncDisplayKit node to UIKit view:
+        var node = new SampleViewNode();
+        var childView = node.View;
+
+        View!.AddSubview(childView);
+
+        // use Auto-Layouting from parent view:
+        childView.TranslatesAutoresizingMaskIntoConstraints = false;
+        View.AddConstraints(new[] {
+            childView.LeftAnchor.ConstraintEqualTo(View.LeftAnchor),
+            childView.RightAnchor.ConstraintEqualTo(View.RightAnchor),
+            childView.CenterYAnchor.ConstraintEqualTo(View.CenterYAnchor),
+            childView.HeightAnchor.ConstraintEqualTo(300),
+        });
+
+        // bind data:
+        var model = new DataModel
+        {
+            Title = "Lorem Ipsum is simply dummy text of the printing and typesetting industry",
+            Image = "https://picsum.photos/200",
+        };
+        node.Update(model);
+
+
+        // udpate data:
+        var timer = new System.Timers.Timer(2000);
+        timer.Elapsed += (sender, e) =>
+        {
+            var additionalText = e.SignalTime.Millisecond % 2 == 0
+                ? " simply dummy text of the printing and typesetting industry"
+                : "";
+            var data = new DataModel
+            {
+                Title = $"Lorem Ipsum is {e.SignalTime.ToString()}{additionalText}",
+                Image = $"https://picsum.photos/200?random={e.SignalTime.Millisecond}",
+            };
+            node.Update(data);
+        };
+        timer.AutoReset = true;
+        timer.Enabled = true;
     }
 
     public override void DidReceiveMemoryWarning()
